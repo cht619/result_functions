6a4d9af0acf7cf8b6e8c521e73f8fc51ad1896db
cht <cccht619@gmail.com>
2021-02-07 09:16:12 +0800

    Standard Deviation by SSDA

--- read_csv.py
+++ read_csv.py
@@ -42,15 +42,35 @@ def get_mean_standard_deviation(root_path, n_data_mean=14):
         all_result.append(result)
 
         print('{}'.format(csv_file[:3]), end=' ')
-
-        # print('{}:{:.3}±{:.1}({:.3}±{:.1})'.
-        #       format(csv_file[:3], np.mean(data_list_Dtl_3[:n_data_mean]) * 100,
-        #           np.std(data_list_Dtl_3[:n_data_mean], ddof=1)* 100,
-        #         np.mean(data_list_Dtl_0[:])* 100, np.std(data_list_Dtl_0[:], ddof=1)* 100))
     print('\n')
     for result in all_result:
         print(result, end='\t')
 
+def get_mean_standard_deviation_SSDA(root_path, n_data_mean=14):
+    csv_files = os.listdir(root_path)
+    all_result = []
+
+    for csv_file in csv_files:
+        with open(r'{}/{}'.format(root_path, csv_file), 'r') as f:
+            csv_reader = csv.reader(f)
+            # n Dtl
+            data_list_Dtl_3 = []
+
+            for i, row in enumerate(csv_reader):
+                data_list_Dtl_3.append(float(row[0]))
+
+        data_list_Dtl_3.sort(reverse=True)
+
+        std_3 = np.std(data_list_Dtl_3[:n_data_mean], ddof=1)* 100
+
+        result = '{:.3}±{:.1f}'.format(np.mean(data_list_Dtl_3[:n_data_mean]) * 100,
+                  std_3)
+        all_result.append(result)
+
+        print('{}'.format(csv_file[:3]), end=' ')
+    print('\n')
+    for result in all_result:
+        print(result, end='\t')
 
 def get_mean_clustering_train(root_path, n_data_mean=10):
 
@@ -93,5 +113,5 @@ def get_mean_clustering_train(root_path, n_data_mean=10):
         # print('{}:{:.3}'.format(csv_file[:3],  np.mean(data_list_Dtl3[:n_data_mean])))
 
 if __name__ == '__main__':
-    get_mean_standard_deviation(r'E:\cht_project\Experimental_Result\ER\Multi_Domain_Sentiment_Dataset\CDAN')
+    get_mean_standard_deviation_SSDA(r'E:\cht_project\Experimental_Result\ER\Multi_Domain_Sentiment_Dataset\SSDA')
     # get_mean_clustering_train(r'E:\cht_project\Experimental_Result\ER\Multi_Domain_Sentiment_Dataset\Clustering_Train\greedy\0.03\Greedy_normalization_max_noDlr')
