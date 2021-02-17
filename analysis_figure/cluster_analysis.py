e44b03deb259c353f0f8726d2c56b6ecf090e8d7
cht <cccht619@gmail.com>
2021-02-17 11:12:24 +0800

    clusters analysis

--- analysis_figure/cluster_analysis.py
+++ analysis_figure/cluster_analysis.py
@@ -17,9 +17,12 @@ import torch.utils.data as data
 import data_path
 from sklearn.cluster import KMeans, SpectralClustering
 from Distance import distance_functions
+from sklearn import metrics
 
 
 colors_src = ['b', 'r', 'g', 'c', 'y', '#9370DB', '#FFFAFA', '#8B0000', '#90EE90', 'orange', '#FF00FF', '#90EE90']
+marker_src = ['o', '*', '^', '<', '>']
+marker_tgt = ['x', '+', 'd']
 
 
 def get_feas_labels(root_path, domain, fea_type='Resnet50'):
@@ -63,11 +66,15 @@ def clustering(feas, labels, n_clusters):
     k = KMeans(n_clusters=n_clusters)
     y_pred = k.fit_predict(feas)
 
+    # print(k.labels_)
+
     # return list
     feas_list = [[feas[i] for i in range(len(feas)) if y_pred[i] == n] for n in range(n_clusters)]
     labels_list = [[labels[i] for i in range(len(labels)) if y_pred[i] == n] for n in range(n_clusters)]
 
-    return feas_list, labels_list
+    print('calinski_harabasz_score:', metrics.calinski_harabasz_score(feas, k.labels_))
+
+    return feas_list, labels_list, k.labels_
 
 
 def get_pairs(d_matrix):
@@ -107,28 +114,81 @@ def plot_original_distribution(root_path, domain_src, domain_tgt, fea_type):
     plt.xticks([])
     plt.yticks([])
     plt.legend(['Ds', 'Dt'])
-    plt.savefig('./clusters.png')
+    plt.savefig('./clusters1.png')
     plt.show()
 
 
-def plot_clusters_distribution(root_path, domain_src, domain_tgt, fea_type='Resnet50', nC_Ds=3, nC_Dt=3):
+def plot_clusters_distribution(root_path, domain_src, fea_type='Resnet50', nC_Ds=5):
+    feas_src, labels_src = get_feas_labels(root_path, domain_src, fea_type)
+    data_tsne_src = TSNE(feas_src)
+    # 不能够重新运行? 不能够单独用每一个类别进行运行，不然会覆盖掉
+
+    feas_src_list, labels_src_list, labels = clustering(feas_src, labels_src, nC_Ds)
+
+    plt.figure(figsize=(12, 12))
+    for i in range(nC_Ds):
+        plt.scatter(data_tsne_src[labels==i][:, 0], data_tsne_src[labels==i][:, 1],
+                    marker=marker_src[i], color=colors_src[i], s=50, alpha=0.5,)
+
+    plt.xticks([])
+    plt.yticks([])
+    plt.legend(['Ds', 'Dt'])
+    plt.savefig('./clusters_kmeans.png')
+    plt.show()
+
+
+def plot_clusters_pairs_distribution(root_path, domain_src, domain_tgt, fea_type='Resnet50', nC_Ds=3, nC_Dt=3):
+    # 一样的颜色就是属于一个pair，形状不同就是不同domain
     feas_src, labels_src = get_feas_labels(root_path, domain_src, fea_type)
     feas_tgt, labels_tgt = get_feas_labels(root_path, domain_tgt, fea_type)
 
-    feas_src_list, labels_src_list = clustering(feas_src, labels_src, nC_Ds)
-    feas_tgt_list, labels_gtg_list = clustering(feas_tgt, labels_tgt, nC_Dt)
+    data_src_tsne = TSNE(feas_src)
+    data_tgt_tsne = TSNE(feas_tgt)
+
+    feas_src_list, labels_src_list, pred_labels_src = clustering(feas_src, labels_src, nC_Ds)
+    feas_tgt_list, labels_gtg_list, pred_labels_tgt = clustering(feas_tgt, labels_tgt, nC_Dt)
 
     distance_matrix = distance_functions.get_distance_matrix(feas_src_list, feas_tgt_list, distance_method='MMD')
 
     pairs = get_pairs(distance_matrix)
+    src_plot_index_list = []
+    legend_src = []
+    legend_tgt = []
 
-    print(pairs)
+    plt.figure(figsize=(12, 12))
+    for i, pair in enumerate(pairs):
+        print(pair)
+        t = plt.scatter(data_tgt_tsne[pred_labels_tgt == pair[1]][:, 0],
+                        data_tgt_tsne[pred_labels_tgt == pair[1]][:, 1],
+                        color=colors_src[pair[0]], marker=marker_tgt[pair[1]])
+        # greedy 重复选取数据
+        if pair[0] not in src_plot_index_list:
+            print(pair[0], src_plot_index_list)
+            s = plt.scatter(data_src_tsne[pred_labels_src==pair[0]][:, 0], data_src_tsne[pred_labels_src==pair[0]][:, 1],
+                        color=colors_src[pair[0]], marker=marker_src[pair[0]])
+            src_plot_index_list.append(pair[0])
+            legend_src.append(s)
+            legend_tgt += ['Ds', 'Dt']
+        else:
+            legend_tgt.append('Dt')
+        legend_src.append(t)
+
+
+    plt.xticks([])
+    plt.yticks([])
+    plt.legend(legend_src, legend_tgt)
+    plt.savefig('./clusters2.png')
+    plt.show()
+
 
 
 
 
 if __name__ == '__main__':
     # plot_original_distribution(data_path.Image_CLEF_root_path, data_path.domain_c, data_path.domain_i, fea_type='Resnet50')
-    plot_clusters_distribution(
+    plot_clusters_pairs_distribution(
         data_path.Image_CLEF_root_path, data_path.domain_c, data_path.domain_i
     )
+    # plot_clusters_distribution(
+    #     data_path.Image_CLEF_root_path, data_path.domain_c
+    # )
