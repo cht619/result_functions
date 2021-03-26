b9e82e5f140a0c1bedab783d9981ca00215c5a44
cht <cccht619@gmail.com>
2021-03-26 13:51:23 +0800

    read csv

--- distance_analysis/kl_divergence_process.py
+++ distance_analysis/kl_divergence_process.py
@@ -25,19 +25,43 @@ def data_process(data_path, all_data=7, all_pairs=7):
     # 实现排序的时候就简单处理了！
     pattern = re.compile(r'(\d\d)±')
 
-    with open(r'{}'.format(r'{}'.format(data_path)), 'r') as f:
+    with open(r'{}'.format(data_path), 'r') as f:
         csv_reader = list(csv.reader(f))
-
         print(csv_reader[0])
-        for i in range(all_data):  # 每一组也有七条数据
-            result = []
-            for j in range(all_pairs): # 7组数据
-                data = csv_reader[6 + i * 7 + 49 * j]
-                # print(data[0], type(data[0]), data[0].split('±'))
-                # print(data[0].split('\\t'))
-                # result.append('{}'.format(j + 2))
-                result.append(data[0])
-                print(data[0])
+        for i in range(all_data):  # Dt有7种情况 2开始
+            dt_pair_data = []  # Dt一样的情况
+            for k in range(all_pairs):  # Dt一样的情况Ds有7种,7种的间隔是49
+                ds_pair_data = []
+                for j in range(1, 6):  # 5个结果
+                    data = csv_reader[j + i * 7 + k * 49][0]
+                    # pair_data.append(csv_reader[j][0])
+                    data = [float(d) for d in data.split('\t')]
+                    ds_pair_data.append(data)
+                dt_pair_mean = np.mean(ds_pair_data, axis=0)  # mean
+                dt_pair_std = np.std(ds_pair_data, axis=0, ddof=1)  # std
+                result = ['{:.2f}±{:.1f}'.format(mean, std) for (mean, std) in zip(dt_pair_mean, dt_pair_std)]
+                print('\t'.join(result))  # 输出7个结果
+                dt_pair_data.append(ds_pair_data)
+            # 计算全局平均
+            dt_pair_data = np.asarray(dt_pair_data)
+            # global_mean = np.mean(dt_pair_data.reshape(-1, i+2), axis=0)
+            # global_std = np.std(dt_pair_data.reshape(-1, i+2), axis=0, ddof=1)
+            # result = ['{:.2f}±{:.1f}'.format(mean ,std) for (mean ,std) in zip(global_mean, global_std)]
+            # print('全局平均:---','\t'.join(result))
+            # 计算全局最小  其实可以都可以在这里设置，但是为了粘贴方便，这里仅仅处理最小值
+            # print('全局最小:---',end='')
+            for col in range(i + 2):
+                minimum_mean = np.mean(dt_pair_data, axis=1)
+                minimum_std = np.std(dt_pair_data, axis=1, ddof=1)
+                # print(minimum_mean)
+                index = np.argmin(minimum_mean[:, col])
+                print('{:.2f}+{:.1f}'.format(minimum_mean[index, col], minimum_std[index, col]), end='\t')
+            print('\n')
+            global_mean = np.mean(dt_pair_data.reshape(-1, i + 2), axis=0)
+            global_std = np.std(dt_pair_data.reshape(-1, i + 2), axis=0, ddof=1)
+            result = ['{:.2f}±{:.1f}'.format(mean, std) for (mean, std) in zip(global_mean, global_std)]
+            print('\t'.join(result))
+            print('\n当前已经结束\n')
 
 
 
