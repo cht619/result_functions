4f02abff8d53e675d6207101aa5f929845b23204
CHT <cccht619@gmail.com>
2022-02-12 08:25:39 +0800

    VisDA

--- Papers/__init__.py
+++ Papers/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-# @Time : 2021/2/7 20:15
+# @Time : 2021/8/14 19:38
 # @Author : CHT
 # @Site : 
 # @File : __init__.py.py
 # @Software: PyCharm 
 # @Blog: https://www.zhihu.com/people/xia-gan-yi-dan-chen-hao-tian
 # @Function:
-
-# -*- coding: utf-8 -*-
--- read_csv.py
+++ read_csv.py
@@ -128,7 +128,7 @@ def get_mean_clustering_train(root_path, n_data_mean=10, all_data=12):
 
             for i in range(len(csv_reader) // all_data):  # 一个文件
                 i = all_data*i
-                data_list = csv_reader[i+2 : i+2+n_data_mean]
+                data_list = csv_reader[i+1 : i+1+n_data_mean]
                 data_list = [float(data[0]) for data in data_list]
                 # get clusters
                 information = csv_reader[i][0]
@@ -151,9 +151,9 @@ def get_mean_clustering_train(root_path, n_data_mean=10, all_data=12):
     for i in domain_name_list:
         print(i, end='\t')
 
-    print('\n')
-    for i in pre_train_result:
-        print('{:.1f}'.format(float(i) * 100), end='\t')
+    # print('\n')
+    # for i in pre_train_result:
+    #     print('{:.1f}'.format(float(i) * 100), end='\t')
 
 
         #     for i, row in enumerate(csv_reader):
@@ -204,13 +204,141 @@ def get_mean_clustering_train_plot(root_path, n_data_mean=10):
         # # print('{}: {:.3}({:.3})'.format(csv_file[:3], np.mean(data_list_Dtl3[:n_data_mean]), np.mean(data_list_Dtl0)))
         # print('{}:{:.3}'.format(csv_file[:3],  np.mean(data_list_Dtl3[:n_data_mean])))
 
+
+def get_mean_standard_deviation_TAT_pseudo_labels(root_path, n_data_mean=5):
+    csv_files = os.listdir(root_path)
+    all_result = []
+
+    for csv_file in csv_files:
+        with open(r'{}/{}'.format(root_path, csv_file), 'r') as f:
+            csv_reader = csv.reader(f)
+            data_list = []
+
+            for i, row in enumerate(csv_reader):
+                # if i == 0:
+                #     continue
+                data_list.append(float(row[4]))
+
+        data_list.sort(reverse=True)
+        std_3 = np.std(data_list[:n_data_mean], ddof=1)
+
+        result = '{:.3}±{:.1f}'.format(np.mean(data_list[:n_data_mean]),
+                  std_3,)
+        all_result.append(result)
+
+        print('{}'.format(csv_file[:-3]), end=' ')
+    print('\n')
+    for result in all_result:
+        print(result, end='\t')
+
+
+def read_mimic3(root_path, n_data_mean=5):
+    csv_files = os.listdir(root_path)
+    all_result = []
+    for csv_file in csv_files:
+        with open(r'{}/{}'.format(root_path, csv_file), 'r') as f:
+            csv_reader = csv.reader(f)
+            data_list = []
+
+            for i, row in enumerate(csv_reader):
+                data_list.append(float(row[-1]))
+
+        data_list.sort(reverse=True)
+        std = np.std(data_list[:n_data_mean], ddof=1)
+        result = '{:.3}±{:.1f}'.format(np.mean(data_list[:n_data_mean])*100,
+                                       std*100, )
+        all_result.append(result)
+        print('{}'.format(csv_file[:-3]), end=' ')
+
+    print('\n')
+    for result in all_result:
+        print(result, end='\t')
+
+
+
+def get_mean_standard_VisDA12(root_path, n_data_mean=5):
+    csv_files = os.listdir(root_path)
+    all_result = []
+
+    for csv_file in csv_files:
+        data = np.loadtxt(os.path.join(root_path, csv_file), delimiter=',')
+        data = data[np.lexsort(-data.T)]  # 根据最后一列的大小进行排序，更多做法再到网上查
+        print(data)
+
+    #     std_3 = np.std(data_list[:n_data_mean], ddof=1)* 100
+    #
+    #     result = '{:.3}±{:.1f}'.format(np.mean(data_list[:n_data_mean]) * 100,
+    #               std_3,)
+    #     # result = '{:.3}±{:.1f}'.format(np.mean(data_list_Dtl_0[:])* 100, std_0)  # TAT
+    #     # result = '{:.3}±{:.1f}'.format(np.mean(data_list[:])* 100, std_3)  # Ours_2
+    #     all_result.append(result)
+    #
+    #     print('{}'.format(csv_file[:-3]), end=' ')
+    # print('\n')
+    # for result in all_result:
+    #     print(result, end='\t')
+
+
+
 if __name__ == '__main__':
-    get_mean_clustering_train(
-        r'E:\cht_project\Experimental_Result\ER\Office_Caltech_DeCAF6\MCADA\4.11', n_data_mean=5, all_data=7)
-    # get_mean_standard_deviation_no_0dtl(r'E:\cht_project\Experimental_Result\DAN\Image_CLEF_Resnet50\MMD_experiment')
-    # get_mean_standard_deviation_SSDA(r'E:\cht_project\Experimental_Result\ER\VisDA_Resnet50\SSDA')
+    get_mean_standard_VisDA12(r'F:\Python_project\Experimental_Result\VisDA\DAN\0211')
     # get_mean_clustering_train(
-    # r'E:\cht_project\Experimental_Result\ER\Office_Home_Resnet50\Clustering_Train\greedy\3.8',
-    # n_data_mean=5, all_data=7)
-    # get_mean_standard_deviation(r'E:\cht_project\Experimental_Result\ER\VisDA_Resnet50\DAN')
-    # get_mean_clustering_train_plot(r'E:\cht_project\Experimental_Result\ER\Office_Home_Resnet50\Clustering_Train\greedy\1.28')
+    #     r'F:\Python_project\Experimental_Result\Office_Caltech\MCADA\pre_train_fine_tune_523', n_data_mean=3, all_data=4)
+    # get_mean_standard_deviation_TAT_pseudo_labels(
+    #     r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\Age\LSTM', n_data_mean=5)
+
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\in_hosptial_mortality\Age\DANN\1130\seq_1', n_data_mean=5)
+    # print('overall_caa')
+    # get_mean_standard_deviation_TAT_pseudo_labels(r'F:\Python_project\Experimental_Result\DG\DAML\1211', n_data_mean=5)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\DANN\Age\1218', n_data_mean=5)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\LSTM\Age\1218', n_data_mean=5)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\in_hosptial_mortality\DANN\Age\1218', n_data_mean=5)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\in_hosptial_mortality\LSTM\Age\1218', n_data_mean=5)
+    #
+    #
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\DANN\Gender\1218', n_data_mean=5)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\LSTM\Gender\1218', n_data_mean=5)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\in_hosptial_mortality\DANN\Gender\1218', n_data_mean=5)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\in_hosptial_mortality\LSTM\Gender\1218', n_data_mean=5)
+
+
+
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\in_hosptial_mortality\AdaRNN\1221', n_data_mean=3)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\AdaRNN\1221', n_data_mean=3)
+
+    # len 24的结果
+    # print('seed3')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\In_hosptial_mortality\LSTM\1226_seed3', n_data_mean=15)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\In_hosptial_mortality\DAN\1226_seed3', n_data_mean=15)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\UNITE_multi\1226_seed0', n_data_mean=3)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\In_hosptial_mortality\FineGrained_TAT\0122', n_data_mean=3)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\In_hosptial_mortality\ST\1230_seed1', n_data_mean=5)
+
+
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\CADA\0121_0\all', n_data_mean=3)
+    # print('\n')
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\In_hosptial_mortality\LSTM\1230_seed1', n_data_mean=5)
+
+
+    # for i in range(0, 11):
+    #     read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\UNITE_multi\1226_seed{}'.format(i), n_data_mean=3)
+    #     print('\n')
+
+
+
+    # read_mimic3(r'F:\Python_project\Experimental_Result\MIMIC3\Phenotyping\Age\LSTM\1129', n_data_mean=5)
+
